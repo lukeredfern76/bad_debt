83f923d5fa2e0dd45a01cc903e4f6148b4dbc3df
lukeredfern76 <144037050+lukeredfern76@users.noreply.github.com>
2024-05-29 14:58:47 +0100

    Add files via upload

---
+++ gap_brand_split.sql
@@ -0,0 +1,10 @@
+create or replace table `data-engineering-prod.u_will_rowe.tbl_Gap_brand_split_Jan24` as (
+
+select distinct 
+Account_no, 
+Payment_Method_Category
+
+from `payg-revenue-assurance.account_gap_model.pa_revenue_reconciliation_master` as r
+
+where 
+  Reporting_Month = '2024-01-01'
